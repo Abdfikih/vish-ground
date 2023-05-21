8760bc6c5e74bdcf3afa3f9aeba322f198bde590
Abdul <abdul.fikih@ui.ac.id>
2023-05-21 19:36:23 +0700

    shift page

--- components/Home/Hero.js
+++ components/Home/Hero.js
@@ -3,7 +3,7 @@ import YourImage from "./drone1.png";
 
 export default function Hero() {
   return (
-    <div className="flex flex-col md:flex-1 md:grid md:grid-cols-2 gap-8 pt-4 md:pt-24 pb-16 px-4 md:px-8 text-white min-h-full items-center p-4 md:py-36 md:h-200 text-center md:text-left pt-2">
+    <div className="flex flex-col md:flex-1 md:grid md:grid-cols-2 gap-8 pt-4 md:pt-24 pb-16 px-4 md:px-16 text-white min-h-full items-center p-4 md:py-36 md:h-200 text-center md:text-left pt-2">
       <div className="flex flex-col justify-center items-center md:items-start md:text-left pt-24">
         <h1 className="text-8xl md:text-12xl font-bold tracking-wide text-transparent bg-gradient-to-r from-blue-400 to-green-400 bg-clip-text">VISH</h1>
         <h1 className="text-4xl md:text-6xl font-bold tracking-wide text-white">GROUND</h1>
@@ -26,7 +26,7 @@ export default function Hero() {
           </button>
         </div>
       </div>
-      <div className="ml-auto mr-[-45px]">
+      <div className="ml-auto mr-[-80px]">
         <Image src={YourImage} alt="Your Image" width={500} height={400} objectFit="contain" />
       </div>
     </div>
