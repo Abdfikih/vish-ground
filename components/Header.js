94f5f7d55e42ed39b85bd6271c1f1e94b994ed27
Abdul <abdul.fikih@ui.ac.id>
2023-05-21 11:51:42 +0700

    edit responsive

--- components/Header.js
+++ components/Header.js
@@ -8,36 +8,36 @@ export default function Header() {
   const [showMenu, setShowMenu] = useState(false);
 
   return (
-    <div className="sticky top-0 md:-top-12 z-50 flex justify-between items-center h-20 md:h-32 px-4 md:px-8 bg-black border-b border-black">
-      <div className="sticky top-0 flex md:justify-between items-center h-20 w-full">
-        <button onClick={() => setShowMenu((prev) => !prev)} className="focus:outline-none block mr-4 md:hidden">
+    <div className="sticky md:-top-2 z-50 flex justify-between items-center h-16 md:h-20 px-2 md:px-4 bg-black border-b border-black">
+      <div className="sticky top-0 flex md:justify-between items-center h-8 w-full">
+        <button onClick={() => setShowMenu((prev) => !prev)} className="focus:outline-none block mr-16 md:hidden z-50">
           {showMenu ? <XIcon className="w-8 h-8 text-white" /> : <MenuIcon className="w-8 h-8 text-white" />}
         </button>
         <h1 className="hidden md:block mr-auto ml-4 text-lg font-medium tracking-wider text-white">VishGround</h1>
         <nav>
           <div
-            className={`fixed top-20 left-0 bottom-0 ${
+            className={`fixed top-0 left-0 bottom-0 ${
               showMenu ? "translate-x-0" : "-translate-x-full"
-            } md:static md:translate-x-0 flex flex-col justify-center md:flex-row gap-10 p-8 w-60 md:w-full md:p-0 bg-black border-r border-gray-200 md:border-0 tracking-wide ease-in-out duration-300`}
+            } md:static md:translate-x-0 flex flex-col justify-center md:flex-row gap-10 p-8 w-60 md:w-full md:p-0 bg-black border-r border-blue-500 md:border-0 tracking-wide ease-in-out duration-300`}
           >
             <div onClick={() => setShowMenu((prev) => !prev)}>
               <Link href="/">
-                <a className="text-white">Home</a>
+                <a className="text-white hover:text-blue-400">Home</a>
               </Link>
             </div>
             <div onClick={() => setShowMenu((prev) => !prev)}>
               <Link href="/">
-                <a className="text-white">About</a>
+                <a className="text-white hover:text-blue-400">About</a>
               </Link>
             </div>
             <div onClick={() => setShowMenu((prev) => !prev)}>
               <Link href="/">
-                <a className="text-white">Service</a>
+                <a className="text-white hover:text-blue-400">Service</a>
               </Link>
             </div>
             <div onClick={() => setShowMenu((prev) => !prev)}>
               <Link href="/">
-                <a className="text-white">Contact Us</a>
+                <a className="text-white hover:text-blue-400">Contact Us</a>
               </Link>
             </div>
           </div>
